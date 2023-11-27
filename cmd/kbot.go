bc5ff926ae9a4612fd0ffd7a9a2530e080f24a71
staskuznetsov <88095654+staskuznetsov@users.noreply.github.com>
2023-11-27 19:48:36 +0000

    Create starting bot

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -20,6 +20,7 @@ var (
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
 	Use:   "kbot",
+	Aliases: []string{"start"},
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
